06d14590e66365937cdc189453c538e6c2a464b5
AMC <53350942+AMC-ai@users.noreply.github.com>
2020-03-10 19:25:07 -0400

    finished 12 and 13 mod a little lost on the files io

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -10,6 +10,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # Note: pay close attention to your current directory when trying to open "foo.txt"
 
 # YOUR CODE HERE
+file = open("foo.txt", "r")
+print(file.read())
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -17,3 +19,36 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+# opens file
+f = open("bar.txt", "w")
+# this adds lines
+f.write('''My precious. 
+Oh my PRECIOUS... 
+No's yous can'ts haves it!!MINE!!''')
+f.close
+# checks for lines
+f = open("bar.txt", "r")
+print(f.read())
+
+# import os
+
+# def read_file(file):
+#     try:
+#         filee = os.path.dirname(__file__)
+
+#         txt = open(f"{filee}/{file}", "r")
+#         read_text = txt.read()
+#         txt.close()
+#         return read_text
+#     except FileExistsError:
+#         print(f"{FileNotFoundError}")
+# print(read_file("foo.txt"))
+
+# fp = open("bar.txt", "w")
+
+# fp.write("""My precious.
+# Oh my PRECIOUS...
+# No's yous can'ts haves it!!MINE!!""")
+
+# # Close the file
+# fp.close()
